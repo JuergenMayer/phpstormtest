6810f36aa4d96736093f5b4dcace2090f6baeed1
econcesslaptop2\nikola_velkovski <juergen.mayer@econcess.de>
2021-08-24 09:59:59 +0200

    Start

---
+++ test.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>A Basic HTML5 Template</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+
+
+</head>
+
+<body>
+  <!-- your content here... -->
+  <h1>test 5</h1>
+
+<?php $message = "Hallo World";
+$rechnung = 2+4;
+echo $message;
+echo $rechnung ?>
+</body>
+</html>
